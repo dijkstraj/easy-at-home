beaf6881a6ac7d0f17b30f0e2a69c9a46f1e9601
Jogchem Dijkstra <jogchem@gmail.com>
2021-01-29 15:00:44 +0100

    Moved a few things to ansible

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ if [ -e /tmp/easy-at-home ]; then
   rm -rf /tmp/easy-at-home
 fi
 git clone https://github.com/dijkstraj/easy-at-home.git /tmp/easy-at-home
-/tmp/easy-at-home/init.sh "$EMAIL"
+sudo apt update && sudo apt install -y ansible
+ansible-playbook /tmp/easy-at-home/main.yml --extra-vars "user=${USER} email=${EMAIL}"
